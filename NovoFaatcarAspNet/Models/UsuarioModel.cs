97a2b4361be9962146a768ffa9153cb52d87ca9f
Murilo Marchesini <julioduartebatista753@gmail.com>
2024-03-26 21:15:09 -0300

    Adicionar arquivos de projeto.

---
+++ NovoFaatcarAspNet/Models/UsuarioModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NovoFaatcarAspNet.Models
+{
+    public class UsuarioModel
+    {
+
+        public int Id { get; set; }      
+
+        [Required(ErrorMessage = "Digite o Nome do funcionario")]
+        [MaxLength(40)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome de Usuário")]
+        [MaxLength(20)]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha desse Usuário")]
+        [MaxLength(20)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Digite um email para recuperação de senha.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é valido!")]
+        [MaxLength(40)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o celular para contato.")]
+        [Phone(ErrorMessage = "O celular informado não é valido.")]
+        [MaxLength(11)]
+        public string Celular { get; set; }
+
+    }
+}
